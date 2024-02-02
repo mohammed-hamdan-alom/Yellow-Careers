7b74d42850d696f295ef1128545a73b118e85f21
Ibrahim <63863032+ibrahimk77@users.noreply.github.com>
2024-02-02 14:53:09 +0000

    Add about field for company model

--- api/models/company.py
+++ api/models/company.py
@@ -2,5 +2,6 @@ from django.db import models
 class Company(models.Model):
     """Model that represents a company"""
     company_name = models.CharField(max_length=100,blank=False)
-    website = models.URLField(max_length=200,blank=True)        
+    website = models.URLField(max_length=200,blank=True)
+    about = models.CharField(max_length=1000,blank=True)  
 
