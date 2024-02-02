5d1ae3604519390ab79afad4665c37d05517dbdf
Ibrahim <63863032+ibrahimk77@users.noreply.github.com>
2024-02-02 14:53:49 +0000

    Add delete method for application

--- api/models/application.py
+++ api/models/application.py
@@ -6,5 +6,14 @@ class Application(models.Model):
     resume = models.ForeignKey('Resume', on_delete=models.CASCADE, null=False)
     job = models.ForeignKey('Job', on_delete=models.CASCADE, null=False)
     date_applied = models.DateField(auto_now_add=True)
+    
     class Meta:
         unique_together = (('job_seeker', 'job'),)
+    
+    def __str__(self):
+        return self.job_seeker.first_name + " " + self.job_seeker.last_name + " - " + self.job.title
+    
+    def delete(self):
+        self.resume.delete()
+        super().delete()
+    
