5728a3cf350f41ee19a12454c201ad0b88fc01cd
Mohammed Alom <mohammed.hamdan.alom@gmail.com>
2024-02-05 14:01:03 +0000

    implement a logout button on an arbritrary page

--- react-frontend/src/pages/HomePage.js
+++ react-frontend/src/pages/HomePage.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import { jwtDecode } from 'jwt-decode'
+import AuthContext from '../context/AuthContext'
 
 const HomePage = () => {
+
+  const {user, logoutUser} = useContext(AuthContext)
+  const token = localStorage.getItem("authTokens")
+
+  if (token) {
+    const decoded = jwtDecode(token)
+    var email = decoded.email
+  }
+
   return (
     <div>
       <h1>
         Hello pookie
       </h1>
+      {token === null &&
+      <>
+        <div>
+          <Link to='/login'>Login</Link>
+        </div>
+        <div>
+          <Link to='/register'>Register</Link>
+        </div>
+      </>
+      }
+      {token !== null &&
+        <div>
+          <a onClick={logoutUser} style={{cursor: 'pointer'}}>Logout</a>
+        </div>
+      }
     </div>
   )
 }
