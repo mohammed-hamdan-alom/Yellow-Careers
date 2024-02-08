17f4edc6a0b0120fb7ec919c17ee58b717daecdc
Ahmad Ramadan <75549950+ahmadramadan0@users.noreply.github.com>
2024-02-08 12:45:25 +0000

    Fix differences caused by merge

--- react-frontend/src/components/login/LoginPage.js
+++ react-frontend/src/components/login/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { Link } from 'react-router-dom';
 import AuthContext from '../../context/AuthContext';
 import './LoginPage.css';
 
