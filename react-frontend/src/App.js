20cc03502ae328144c5a4227088b60700f989b7a
Omar Ahmad <omar.1.ahmad@kcl.ac.uk>
2024-02-10 22:05:24 +0000

    Add Profile and Resume Pages

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import LoginPage from "./components/login/LoginPage";
 import Register from './components/register/Register';
 import { AuthProvider } from './context/AuthContext';
@@ -8,22 +8,26 @@ import SearchPage from "./components/search/SearchPage";
 import LandingPage from "./components/landing_page/LandingPage";
 // import PrivateRoute from './utils/PrivateRoute';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import ProfilePage from "./components/profile/ProfilePage";
+import ResumePage from "./components/resume/ResumePage";
 
 
 function App() {
   return (
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path='/register' element={<Register />} />
-          <Route path="/job-seeker/*" element={<JobSeekerLayout />}>
-            <Route path="dashboard" element={<DashBoardPage />} />
-            <Route path="search" element={<SearchPage />} />
-          </Route>
-          {/* <PrivateRoute component={Dashboard} path='/dashboard'/> */}
-        </Routes>
-      </AuthProvider>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/job-seeker/*" element={<JobSeekerLayout />}>
+          <Route path="dashboard" element={<DashBoardPage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="resume" element={<ResumePage />} />
+          <Route path="profile" element={<ProfilePage />} />
+        </Route>
+        {/* <PrivateRoute component={Dashboard} path='/dashboard'/> */}
+      </Routes>
+    </AuthProvider>
   );
 }
 
