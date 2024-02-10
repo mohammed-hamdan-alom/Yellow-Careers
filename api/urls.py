f1b85e3dc209f6ab05c8c6592ec3ad3a182cb860
Nithursan Muraleetharan <lonketh@gmail.com>
2024-02-10 11:38:51 +0000

    Refactor urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import hello_world_views, MyTokenObtainPairView, RegisterView, test_api_endpoint
+from .views import hello_world_views, MyTokenObtainPairView, RegisterView, test_api_endpoint, jobs_creation_view
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import hello_world_views, jobs_creation_view
+
 urlpatterns = [
     path('hello-world/', hello_world_views.hello_world, name='hello_world'),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
