337fe12683d302603cd0a12894ce16c9b63dee69
Nithursan Muraleetharan <lonketh@gmail.com>
2024-02-10 11:40:09 +0000

    Fix urls.py

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
-    path('dashboard/', test_api_endpoint, name="test")
+    path('dashboard/', test_api_endpoint, name="test"),
     path('jobs/create-job', jobs_creation_view.job_create, name='create_job'),
     path('jobs/all-jobs', jobs_creation_view.job_list, name='all_jobs'),
 ]
