77c8ca243743eaabe2419d73e6de0eb08cdfe27e
Ibrahim <63863032+ibrahimk77@users.noreply.github.com>
2024-02-18 19:57:46 +0000

    Display error messages

--- react-frontend/src/components/resume/ResumePage.js
+++ react-frontend/src/components/resume/ResumePage.js
@@ -15,6 +15,16 @@ function UpdateResumePage() {
 
   const [resumeId, setResumeId] = useState(null);
 
+  const [errors, setErrors] = useState({
+    github: '',
+    linkedin: '',
+    about: '',
+    experience: '',
+  });
+
+
+  
+// Decode token to get user information
   let authTokens = localStorage.getItem("authTokens");
   let token;
   let decodedToken;
@@ -30,9 +40,9 @@ function UpdateResumePage() {
   } else {
     console.log("No authTokens found");
   }
-
   let userId = decodedToken.user_id;
 
+  // Fetch user's resume id
   useEffect(() => {
     async function fetchResumeId() {
       const response = await fetch(`http://localhost:8000/api/job-seekers/${userId}/`);
@@ -47,10 +57,9 @@ function UpdateResumePage() {
       setResumeId(id);
     });
   }, [userId]);
-  
-  // resumeId = fetchResumeId();
   console.log('resume',resumeId);
 
+  // Fetch tthe user's resume details
   useEffect(() => {
     if (resumeId === null) {
       return;
@@ -68,6 +77,7 @@ function UpdateResumePage() {
     });
   };
 
+  // PUT request when form submitted
   const handleSubmit = (event) => {
     event.preventDefault();
     AxiosInstance.put(`http://localhost:8000/api/resumes/${resumeId}/update/`, {
@@ -87,8 +97,21 @@ function UpdateResumePage() {
           showConfirmButton: false,
       });
 
+
+      setErrors({
+        github: '',
+        linkedin: '',
+        about: '',
+        experience: '',
+      });
+
     }).catch((error) => {
         console.error('Error:', error);
+        let errorMessages = '';
+        if (error.response && error.response.data) {
+          // Parse the error response
+          errorMessages = Object.values(error.response.data).join(' ');
+          setErrors(error.response.data);};
         swal.fire({
           title: "Update Failed",
           icon: "error",
@@ -108,24 +131,28 @@ return (
       <label>
         GitHub:
         <input type="text" name="github" value={resume.github} onChange={handleChange} />
+        {errors.github && <p>{errors.github}</p>}
       </label>
     </div>
     <div>
       <label>
         LinkedIn:
         <input type="text" name="linkedin" value={resume.linkedin} onChange={handleChange} />
+        {errors.linkedin && <p>{errors.linkedin}</p>}
       </label>
     </div>
     <div>
       <label>
         About:
         <textarea name="about" value={resume.about} onChange={handleChange} />
+        {errors.about && <p>{errors.about}</p>}
       </label>
     </div>
     <div>
       <label>
         Experience:
         <textarea name="experience" value={resume.experience} onChange={handleChange} />
+        {errors.experience && <p>{errors.experience}</p>}
       </label>
     </div>
     <button type="submit">Update Resume</button>
