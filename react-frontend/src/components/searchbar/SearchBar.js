ec8a80129cd81fe5b84b3b9ec0a3c88619098187
Nithursan Muraleetharan <lonketh@gmail.com>
2024-02-21 21:32:29 +0000

    Change search bar to auto-update

--- react-frontend/src/components/searchbar/SearchBar.js
+++ react-frontend/src/components/searchbar/SearchBar.js
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from "react";
 import JobSummary from "../job_summary/JobSummary";
-const SearchDatabase = ({ database }) => {
+const SearchBar = ({ database }) => {
     const [query, setQuery] = useState('');
     const [results, setResults] = useState([]);
 
-    const handleSearch = () => {
+    const handleSearch = (e) => {
+        const query = e.target.value
+        setQuery(query)
         const results = database.filter(item => item.title.toLowerCase().startsWith(query.toLowerCase()));
         setResults(results);
-    };
-
-    const handleChange = event => {
-        setQuery(event.target.value);
     };
 
     useEffect(() => {
@@ -21,24 +19,23 @@ const SearchDatabase = ({ database }) => {
         <div>
             <input type="text"
                 value={query}
-                onChange={handleChange} />
-            <button onClick={() => handleSearch(database)}>Search</button>
+                onChange={handleSearch} />
             <ul>
-            <ul className='job-summary'>
-                {results.map(job => (
-                    <JobSummary
-                        key={job.id}
-                        id={job.id}
-                        title={job.title}
-                        hirer={job.hirer}
-                        description={job.description}
-                        location={job.address}
-                    />
-                ))}
-            </ul>
+                <ul className='job-summary'>
+                    {results.map(job => (
+                        <JobSummary
+                            key={job.id}
+                            id={job.id}
+                            title={job.title}
+                            hirer={job.hirer}
+                            description={job.description}
+                            location={job.address}
+                        />
+                    ))}
+                </ul>
             </ul>
         </div>
     );
 };
 
-export default SearchDatabase;
+export default SearchBar;
