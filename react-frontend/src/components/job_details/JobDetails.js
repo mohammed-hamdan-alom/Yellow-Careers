987e40a8795d467e7daacecde644225a5f04f70d
Muhammad0527 <102260144+Muhammad0527@users.noreply.github.com>
2024-02-25 10:35:36 +0000

    Complete application page

--- react-frontend/src/components/job_details/JobDetails.js
+++ react-frontend/src/components/job_details/JobDetails.js
@@ -26,61 +26,45 @@ function JobDetails () {
     useEffect(() => {
         Promise.all([
             AxiosInstance.get(`api/jobs/${jobId}/`), // get the job details
-            AxiosInstance.get(`api/jobs/${jobId}/questions/`), // get the questions for the job
-            AxiosInstance.get(`api/job-seeker/${userId}/resume/`), // get the resume data of the job seeker
+            AxiosInstance.get(`api/jobs/${jobId}/company/`), // get the company data of the job
             AxiosInstance.get(`api/jobs/${jobId}/address/`), // get the address data of the job
-            AxiosInstance.get(`api/jobs/${jobId}/company/`) // get the company data of the job
+            AxiosInstance.get(`api/jobs/${jobId}/questions/`), // get the questions for the job
+            AxiosInstance.get(`api/job-seeker/${userId}/resume/`) // get the resume data of the job seeker
         ]).then((responses) => {
             setJob(responses[0].data);
-            setQuestions(responses[1].data);
-            setResume(responses[2].data);
-            setAddress(responses[3].data);
-            setCompany(responses[4].data);
+            setCompany(responses[1].data);
+            setAddress(responses[2].data);
+            setQuestions(responses[3].data);
+            setResume(responses[4].data);
         }).catch((error) => console.error('Error fetching data:', error));
     }, [jobId, userId]);
 
+    // check if the job is saved
     useEffect(() => {
         AxiosInstance.get(`api/job-seeker/${userId}/saved-jobs/`)
             .then((res) => {
                 setSavedJobs(res.data);
-                setIsJobSaved(res.data.some(savedJob => String(savedJob.id) === String(jobId)))
-        }).catch((error) => console.error('Error fetching data:', error)); // check if the job is saved
+        }).catch((error) => console.error('Error fetching data:', error));
     }, [isJobSaved]);
-
-    // this is to save the answer to the database
-    const createAnswers = (applicationId) => {
-        // send a POST request for each answer
-        for (const questionId in answers) {
-            AxiosInstance.post(`api/answers/create/`, {
-                application: applicationId,  
-                question: questionId,
-                answer: answers[questionId],
-            })
-            .catch((error) => console.error('Error saving answer:', error));
-        }
-    };
-
 
     // this is for applying for the job
     const handleApply = () => {
         if(questions.length === 0) {
-            const applicationData = {
-                job_seeker: userId,
-                job: jobId,
-                resume: resume.id,
-            }
-            
-            // send a POST request to create the application
-            AxiosInstance.post('api/applications/create/', applicationData)
-            .then((response) => {
-                const application = response.data;
-                console.log('Created application:', application);
-
-                // the application's ID is now available as application.id
-                createAnswers(application.id);
-            })
-            .catch((error) => console.error('Error creating application:', error));
-        }
+            if (window.confirm('Are you sure you want to apply for this job?')) {
+                const applicationData = {
+                    job_seeker: userId,
+                    job: jobId,
+                    resume: resume.id,
+                }
+                
+                // send a POST request to create the application
+                AxiosInstance.post('api/applications/create/', applicationData)
+                .then((response) => {
+                    const application = response.data;
+                    console.log('Created application:', application);
+                })
+                .catch((error) => console.error('Error creating application:', error));
+            }}
 
         else {
             navigate(`questions/`)
@@ -89,23 +73,22 @@ function JobDetails () {
 
     const handleSave = () => {
         const savedJob = savedJobs.find(savedJob => String(savedJob.id) === String(jobId));
-        console.log('Saved job:', savedJob);
         if (savedJob) {
-            // If the job is already saved, unsave it
+            // if the job is already saved, unsave it
             AxiosInstance.delete(`api/saved-jobs/update/${userId}/${jobId}/`)
                 .then(() => {
-                    // Remove the unsaved job from the state
+                    // set the state to false
                     setIsJobSaved(false);
                 })
                 .catch((error) => console.error('Error unsaving job:', error));
         } else {
-            // If the job is not saved, save it
+            // if the job is not saved, save it
             AxiosInstance.post(`api/saved-jobs/create/`, {
                 job_seeker: userId,
                 job: jobId,
             })
             .then((response) => {
-                // Add the saved job to the state
+                // set the state to true
                 setIsJobSaved(true);
             })
             .catch((error) => console.error('Error saving job:', error));
@@ -117,7 +100,6 @@ function JobDetails () {
             <h1>{job.title}</h1>
             <h2>{address.country}</h2>
             <h3>{company.company_name}</h3>
-            {/* dispay each question and allow an answer */}
             <button onClick={handleApply}>Apply</button>
             {isJobSaved ? (
                 <button onClick={handleSave}>Unsave</button>
--- react-frontend/src/components/job_details/JobQuestions.js
+++ react-frontend/src/components/job_details/JobQuestions.js
@@ -5,17 +5,58 @@ import AxiosInstance from '../../Axios';
 
 
 function JobQuestions() {
+    const { user } = useContext(AuthContext);
+    const userId = user.user_id;
     const { jobId } = useParams();
     const [questions, setQuestions] = useState([]);
     const [answers, setAnswers] = useState({}); // this is for the answers
+    const [resume, setResume] = useState({});   // this is for the resume
   
     useEffect(() => {
-      AxiosInstance.get(`api/jobs/${jobId}/questions/`)
-        .then((response) => {
-          setQuestions(response.data);
-        })
-        .catch((error) => console.error('Error getting questions:', error));
-    }, [jobId]);
+        Promise.all([
+            AxiosInstance.get(`api/jobs/${jobId}/questions/`), // get the questions for the job
+            AxiosInstance.get(`api/job-seeker/${userId}/resume/`) // get the resume data of the job seeker
+        ]).then((responses) => {
+            setQuestions(responses[0].data);
+            setResume(responses[1].data);
+        }).catch((error) => console.error('Error fetching data:', error));
+    }, [jobId, userId]);
+
+
+    const createAnswers = (applicationId) => {
+        // send a POST request for each answer
+        for (const questionId in answers) {
+            AxiosInstance.post(`api/answers/create/`, {
+                application: applicationId,  
+                question: questionId,
+                answer: answers[questionId],
+            })
+            .catch((error) => console.error('Error saving answer:', error));
+        }
+    };
+
+        // this is for applying for the job
+    const handleApply = () => {
+        if (window.confirm('Are you sure you want to apply for this job?')) {
+            const applicationData = {
+                job_seeker: userId,
+                job: jobId,
+                resume: resume.id,
+            }
+            
+            // send a POST request to create the application
+            AxiosInstance.post('api/applications/create/', applicationData)
+            .then((response) => {
+                const application = response.data;
+                console.log('Created application:', application);
+
+                // the application's ID is now available as application.id
+                createAnswers(application.id);
+            })
+            .catch((error) => console.error('Error creating application:', error));
+        }
+    };
+
 
 
     const handleInputChange = (questionId, newValue) => {
@@ -38,6 +79,7 @@ function JobQuestions() {
                     />
                 </div>
             ))}
+            <button onClick={handleApply}>Apply</button>
       </div>
     );
   }
