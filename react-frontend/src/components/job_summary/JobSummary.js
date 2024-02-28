e36d73e76aa111c421263434aaff54a27535af88
Nithursan Muraleetharan <lonketh@gmail.com>
2024-02-28 15:50:28 +0000

    Fix files to allow jest to run tests

--- react-frontend/src/components/job_summary/JobSummary.js
+++ react-frontend/src/components/job_summary/JobSummary.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './JobSummary.css';
-import AxiosInstance from '../../Axios';
+import AxiosInstance from '../Axios';
 
 const JobSummary = ({ job }) => {
   const navigate = useNavigate();
