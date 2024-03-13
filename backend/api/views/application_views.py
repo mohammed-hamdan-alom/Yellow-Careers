0514efc9875b827a4831b59402843636ca1616cb
Ibrahim <63863032+ibrahimk77@users.noreply.github.com>
2024-03-13 16:48:26 +0000

    Add restrictions to views

--- backend/api/views/application_views.py
+++ backend/api/views/application_views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from api.models import Application
+from api.models import Application, EmployerJobRelation,Employer
 from api.serializers.application_serializer import ApplicationSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.exceptions import PermissionDenied
@@ -13,9 +13,24 @@ class ApplicationListView(BaseApplicationView, generics.ListAPIView):
 
 class ApplicationRetrieveView(BaseApplicationView, generics.RetrieveAPIView):
     def get_object(self):
+        user = self.request.user
         obj = super().get_object()
-        if not obj.job_seeker.id == self.request.user.id:
-            raise PermissionDenied("You do not have permission to view this application.")
+        
+        if hasattr(user, 'jobseeker'):
+            if not obj.job_seeker.id == user.id:
+                raise PermissionDenied("You do not have permission to view this application.")
+        else:
+            job_id = obj.job.id
+            employer = Employer.objects.get(user_ptr_id = user.id)
+            employer_ids = EmployerJobRelation.objects.filter(job_id=job_id).values_list('employer', flat=True)
+            job_company = Employer.objects.get(id=employer_ids[0]).company
+
+            if not employer.company == job_company:
+                raise PermissionDenied("You do not have permission to view this application.")
+            if not employer.is_company_admin:
+                if not user.id in employer_ids:
+                    raise PermissionDenied("You do not have permission to view this application.")
+        print(obj.resume.github)
         return obj
 
 class JobSeekerApplicationRetrieveView(BaseApplicationView, generics.RetrieveAPIView):
