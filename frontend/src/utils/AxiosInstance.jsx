0bdd60a2d418d734807559e981ed641153fff66d
Ibrahim <63863032+ibrahimk77@users.noreply.github.com>
2024-03-15 08:37:22 +0000

    Update AxiosInstance.jsx
    Add a check if a token exists

--- frontend/src/utils/AxiosInstance.jsx
+++ frontend/src/utils/AxiosInstance.jsx
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
+const authTokens = JSON.parse(localStorage.getItem('authTokens'));
+const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+};
+
+if (authTokens && authTokens.access) {
+    headers['Authorization'] = `Bearer ${authTokens.access}`;
+}
+
 const AxiosInstance = axios.create({
     baseURL: 'http://127.0.0.1:8000',
     timeout:10000,
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': `Bearer ${ JSON.parse(localStorage.getItem('authTokens')).access}`,
-    }
+    headers: headers
 })
 
 export default AxiosInstance;
