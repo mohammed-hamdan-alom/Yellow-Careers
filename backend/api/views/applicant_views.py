a8cdcdff56030a76133309943d93fab85db7470f
Ibrahim <63863032+ibrahimk77@users.noreply.github.com>
2024-03-16 07:53:22 +0000

    Update matchmaker
    Implement word2vec model to calculate similarity between jobs and resumes and produce a score
    Add a matchmaker for applicant lists
    Add a score to factor in location

--- backend/api/views/applicant_views.py
+++ backend/api/views/applicant_views.py
@@ -2,6 +2,8 @@ from rest_framework import generics
 from api.models import JobSeeker, Job, Application
 from api.serializers import JobSeekerSerializer
 from django.shortcuts import get_object_or_404
+from api.matchmaker.matchmaker import *
+
 
 
 class BaseApplicantView:
@@ -12,7 +14,7 @@ class ApplicantListView(BaseApplicantView, generics.ListAPIView):
     def get_queryset(self):
         job_id = self.kwargs["pk"]
         applications = Application.objects.filter(job_id = job_id)
-        return [application.job_seeker for application in applications]
+        return getMatchedApplicantsForJob(Job.objects.get(id=job_id), [application.job_seeker for application in applications])
 
 class JobSeekerRetrieveView(BaseApplicantView, generics.RetrieveAPIView):
     pass
--- backend/api/views/job_views.py
+++ backend/api/views/job_views.py
@@ -7,6 +7,8 @@ from api.models import Job, Application, JobSeeker, SavedJobs, Employer, Employe
 
 from api.serializers.job_serializer import JobSerializer
 
+from api.matchmaker.matchmaker import *
+
 from Levenshtein import ratio
 
 class JobCreationView(generics.CreateAPIView):
@@ -32,15 +34,7 @@ class JobSeekerMatchedJobsListingView(generics.ListAPIView):
 	def get_queryset(self):
 		job_seeker_id = self.kwargs['pk']
 		job_seeker = get_object_or_404(JobSeeker, id=job_seeker_id)
-		job_scores = {}
-		for job in Job.objects.all():
-			job_scores[job] = ratio(job.to_string(), job_seeker.get_resume().to_string())
-		matched_order = sorted(job_scores.items(), key=lambda x: x[1], reverse=True)
-		matched_jobs = [item[0] for item in matched_order]
-		applications = Application.objects.filter(job_seeker=job_seeker)
-		applied_jobs = [application.job for application in applications]
-		return list(filter(lambda x: x not in applied_jobs, matched_jobs))
-		# return Job.objects.exclude(id__in=[job.id for job in applied_jobs])
+		return getMatchedJobsForJobSeeker(job_seeker)
 	
 class JobSeekerSavedJobsListView(generics.ListAPIView):
 	'''Get the jobs saved by a job seeker. The job seeker id is passed as a parameter in the url.'''
