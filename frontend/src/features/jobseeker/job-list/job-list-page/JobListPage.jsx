76cabe4b1c008237ab27ac686512f7d51e5c50be
ibrahimuddin <ibrahimu0203@gmail.com>
2024-03-21 17:25:49 +0000

    fix fetch bug initial attempt

--- frontend/src/features/jobseeker/job-list/job-list-page/JobListPage.jsx
+++ frontend/src/features/jobseeker/job-list/job-list-page/JobListPage.jsx
@@ -13,22 +13,36 @@ const JobListPage = () => {
   const [isJobRetrieved, setIsJobRetrieved] = useState(false);
 
   useEffect(() => {
-    const fetchResumeAndJobs = async () => {
+    const fetchResumeAndJobs = async (retryCount = 0) => {
       try {
         const response = await AxiosInstance.get(`api/job-seeker/${userId}/resume/`);
         setResume(response.data);
+        
         if (response.data.id !== undefined) {
           const res = await AxiosInstance.get(`api/job-seeker/${userId}/matched-jobs/`);
           setJobs(res.data);
-          setIsJobRetrieved(true);
+          setJobRetrieved(true);
         }
       } catch (error) {
         console.error("Error fetching data:", error);
+        // Retry logic
+        const maxRetries = 10;
+        if (retryCount < maxRetries) {
+          const delay = 2000; // 2 seconds delay, adjust as needed
+          console.log(`Retrying after ${delay} milliseconds...`);
+          setTimeout(() => {
+            fetchResumeAndJobs(retryCount + 1); // Retry with incremented retryCount
+          }, delay);
+        } else {
+          console.error("Max retries exceeded. Unable to fetch data.");
+          // Handle max retries exceeded, maybe display an error message
+        }
       }
     };
-
+  
     fetchResumeAndJobs();
-  }, [isJobRetrieved]);
+  }, [userId]);
+  
 
   return (
     <div className="flex flex-col justify-center">
