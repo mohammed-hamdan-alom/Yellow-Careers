290422de5040fadd76cc363075f87293225d5a16
Nithursan Muraleetharan <nithursan.muraleetharan@kcl.ac.uk>
2024-03-23 15:13:37 +0000

    Update JobApplicants.test.jsx

--- frontend/src/features/employer/job_applicants/tests/JobApplicants.test.jsx
+++ frontend/src/features/employer/job_applicants/tests/JobApplicants.test.jsx
@@ -10,7 +10,7 @@ const data = [
         "id": 54,
         "date_applied": "2024-03-18",
         "status": "U",
-        "decision": "U",
+        "decision": "A",
         "job_seeker": 27,
         "resume": 154,
         "job": 7
@@ -18,7 +18,7 @@ const data = [
     {
         "id": 68,
         "date_applied": "2024-03-18",
-        "status": "U",
+        "status": "R",
         "decision": "U",
         "job_seeker": 60,
         "resume": 168,
@@ -71,11 +71,6 @@ describe('JobApplicants component', () => {
 
     afterEach(cleanup);
 
-    test("renders ApplicationSummary component", async () => {
-        const component = await screen.getAllByTestId("mock-applicationsummary")[0];
-        expect(component).toBeInTheDocument();
-    });
-
     test("renders title", async () => {
         const title = await screen.findByText("Matched applicants");
         expect(title).toBeInTheDocument();
@@ -87,17 +82,53 @@ describe('JobApplicants component', () => {
     });
 
     test("renders Job Details button", async () => {
-        const button = await screen.findByRole("button");
+        const button = await screen.findByText("Job Details");
         expect(button).toBeInTheDocument;
-        expect(button).toHaveTextContent("Job Details")
     });
 
     test("Job Details button redirects correctly", async () => {
-        const button = await screen.findByRole("button");
+        const button = await screen.findByText("Job Details");
         await act(async () => {
             fireEvent.click(button)
         })
         expect(navigate).toHaveBeenCalledWith(`/employer/job-details/7`)
     });
+
+    test("Status filter renders and filters correctly", async () => {
+        const statusLabel = await screen.getByText("Status Filter:")
+        const statusFilter = await screen.getByTestId("status").querySelector("input")
+        expect(statusLabel).toBeInTheDocument();
+        expect(statusFilter).toBeInTheDocument();
+
+        await act(async () => {
+            fireEvent.change(statusFilter, { target: { value: "R" } })
+            const read = screen.getByText("Read")
+
+            fireEvent.click(read)
+        })
+
+        const applications = await screen.getAllByTestId("mock-applicationsummary");
+        expect(applications).toHaveLength(1)
+
+    })
+
+
+    test("Decision filter renders and filters correctly", async () => {
+        const decisionLabel = await screen.getByText("Decision Filter:")
+        const decisionFilter = await screen.getByTestId("decision").querySelector("input")
+        expect(decisionLabel).toBeInTheDocument();
+        expect(decisionFilter).toBeInTheDocument();
+
+        await act(async () => {
+            fireEvent.change(decisionFilter, { target: { value: "A" } })
+            const accepted = screen.getByText("Accepted")
+
+            fireEvent.click(accepted)
+        })
+
+        const applications = await screen.getAllByTestId("mock-applicationsummary");
+        expect(applications).toHaveLength(1)
+
+    })
 
 });
