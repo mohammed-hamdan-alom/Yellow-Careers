45b71cac8db2a7a8528600e372f3a8275a8447ea
Nithursan Muraleetharan <lonketh@gmail.com>
2024-03-26 19:56:48 +0000

    Fix GetStartedPage tests

--- frontend/src/features/authentication/GetStartedPage/__tests__/GetStartedPage.test.jsx
+++ frontend/src/features/authentication/GetStartedPage/__tests__/GetStartedPage.test.jsx
@@ -1,17 +1,37 @@
 import React from "react";
 import { vi } from "vitest";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, act } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 import GetStartedPage from "../GetStartedPage";
+import { AuthProvider } from "@/context/AuthContext";
+
+const navigate = vi.fn();
+
+vi.mock("react-router-dom", async (importOriginal) => {
+  const actual = await importOriginal();
+  return {
+    ...actual,
+    useNavigate: () => navigate,
+  };
+});
+
+// vi.mock("@/components/ui/button", () => ({
+//   Button: ({ children }) => <button data-testid="mock-button">{children}</button>,
+// }));
 
 describe("GetStarted component", () => {
+  beforeEach(async () => {
+    await act(async () => {
+      render(
+        <MemoryRouter>
+          <GetStartedPage />
+        </MemoryRouter>,
+      );
+    })
+  })
   test("renders without crashing", () => {
-    render(
-      <MemoryRouter>
-        <GetStartedPage />
-      </MemoryRouter>,
-    );
+
     // Assert that the component renders without throwing any errors
     expect(screen.getByText(/Get started as a Job seeker or as an Employer/i)).toBeInTheDocument();
     expect(screen.queryByLabelText(/Job Seeker/i)).not.toBeChecked();
@@ -19,50 +39,38 @@ describe("GetStarted component", () => {
   });
 
   test("renders radio buttons correctly", () => {
-    render(
-      <MemoryRouter>
-        <GetStartedPage />
-      </MemoryRouter>,
-    );
+
     // Assert that radio buttons for both options are rendered
     expect(screen.getByLabelText(/Job Seeker/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/Employer/i)).toBeInTheDocument();
   });
 
   test("handles option change correctly", () => {
-    render(
-      <MemoryRouter>
-        <GetStartedPage />
-      </MemoryRouter>,
-    );
-    const optionOne = screen.getByTestId("jobseeker");
+
+    const optionOne = screen.getByTestId("job-seeker");
     const optionTwo = screen.getByTestId("employer");
 
-    userEvent.click(optionOne);
+    fireEvent.click(optionOne);
 
-    expect(screen.getByTestId("jobseeker")).toBeChecked();
+    expect(screen.getByTestId("job-seeker")).toBeChecked();
     expect(screen.getByTestId("employer")).not.toBeChecked();
 
-    userEvent.click(optionTwo);
+    fireEvent.click(optionTwo);
 
     expect(screen.getByLabelText(/Employer/i)).toBeChecked();
     expect(screen.queryByLabelText(/Job Seeker/i)).not.toBeChecked();
   });
 
-  test("handles form submission correctly", () => {
-    const navigate = vi.fn();
-    render(
-      <MemoryRouter>
-        <GetStartedPage />
-      </MemoryRouter>,
-    );
-    // Mock the useNavigate hook
-    vi.spyOn(require("react-router-dom"), "useNavigate").mockReturnValue(navigate);
+  test("handles form submission correctly", async () => {
+
     // Select the radio button for "Employer"
-    fireEvent.click(screen.getByLabelText(/Employer/i));
+    fireEvent.click(screen.getByTestId("employer"));
     // Click the submit button
-    userEvent.click(screen.getByText(/Go/i));
+    const submitButton = screen.getByTestId("submit");
+    await act(async () => {
+      fireEvent.click(submitButton);
+    })
     // Assert that the handleSubmit function is called
-    expect(navigate).toHaveBeenCalledWith("/auth/register-employer");
+    expect(navigate).toHaveBeenCalledWith("/auth/join-or-create-company");
   });
 });
