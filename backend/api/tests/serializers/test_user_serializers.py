127f59caa470b63c1b95019f53d2590d96e2a059
ibrahimuddin <ibrahimu0203@gmail.com>
2024-03-26 15:54:03 +0000

    jobseeker and user serializer tests

--- backend/api/tests/serializers/test_user_serializers.py
+++ backend/api/tests/serializers/test_user_serializers.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
-from api.models import User
+from api.models import User, Company, JobSeeker
 from api.serializers import UserSerializer, MyTokenObtainPairSerializer, EmployerRegisterSerializer, JobSeekerRegisterSerializer, ChangePasswordSerializer
 from rest_framework.exceptions import ValidationError
+from rest_framework.test import APIRequestFactory
+
 
 class UserSerializerTest(TestCase):
 
@@ -49,29 +51,54 @@ class UserSerializerTest(TestCase):
         # Ensure the token contains the expected claims
         self.assertEqual(token['email'], self.user.email)
 
-# class MyTokenObtainPairSerializerTest(TestCase):
-#     def setUp(self):
-#         self.user = User.objects.get(email='johndoe@example.com')
-#         self.serializer2 = MyTokenObtainPairSerializer(data={'email': 'test@test.com'})
-
-#     def test_contains_expected_fields(self):
-#         self.serializer2.is_valid()
-#         token = self.serializer2.validated_data
-#         self.assertIn('email', token)
-#         self.assertIn('first_name', token)
-#         self.assertIn('last_name', token)
-#         self.assertIn('phone_number', token)
-
-class RegisterSerializerTest(TestCase):
+class EmployerRegisterSerializerTest(TestCase):
     def setUp(self):
         self.client = APIClient()
+        self.company = Company.objects.create(company_name="random")
         self.valid_payload = {
             'email': 'test@test.com',
             'password': 'testpassword123',
             'password2': 'testpassword123',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'other_names': 'random',
+            'phone_number': '1234567890',
+            'company': self.company.id,
+            'is_company_admin': True
         }
 
-    def test_create_user(self):
+    def test_create_employer(self):
+        serializer = EmployerRegisterSerializer(data=self.valid_payload)
+        if serializer.is_valid():
+            employer = serializer.save()
+            self.assertIsInstance(employer, User)
+            self.assertEqual(employer.email, self.valid_payload['email'])
+            self.assertTrue(employer.check_password(self.valid_payload['password']))
+    
+    def test_passwords_not_match(self):
+        self.valid_payload['password2'] = 'wrongpassword'
+        serializer = EmployerRegisterSerializer(data=self.valid_payload)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('password', serializer.errors)
+        self.assertEqual(serializer.errors['password'][0], 'Password fields do not match')
+
+class JobSeekerRegisterSerializerTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.valid_payload = {
+            'email': 'test@test.com',
+            'password': 'Testpassword123_',
+            'password2': 'Testpassword123_',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'other_names' : 'random',
+            'dob': '1990-01-01',
+            'phone_number': '1234567890',
+            'nationality': 'Country',
+            'sex': 'M'
+        }
+
+    def test_create_jobseeker(self):
         serializer = JobSeekerRegisterSerializer(data=self.valid_payload)
         if serializer.is_valid():
             user = serializer.save()
@@ -83,30 +110,104 @@ class RegisterSerializerTest(TestCase):
         self.valid_payload['password2'] = 'wrongpassword'
         serializer = JobSeekerRegisterSerializer(data=self.valid_payload)
         self.assertFalse(serializer.is_valid())
-        ##self.assertEqual(serializer.errors['password'][0], 'Password fields do not match')
+        self.assertEqual(serializer.errors['password'][0], 'Password fields do not match')
 
+    def test_update_jobseeker_password_match(self):
+        jobseeker = JobSeeker.objects.create(
+            email='test@test.com',
+            first_name='John',
+            last_name='Doe',
+            other_names='random',
+            dob='1990-01-01',
+            phone_number='1234567890',
+            nationality='Country',
+            sex='M'
+        )
+        update_payload = {
+            'email': jobseeker.email,
+            'first_name': jobseeker.first_name,
+            'last_name': jobseeker.last_name,
+            'dob': jobseeker.dob,
+            'phone_number': jobseeker.phone_number,
+            'nationality': jobseeker.nationality,
+            'sex': jobseeker.sex,
+            'password': 'NewTestpassword123_',
+            'password2': 'NewTestpassword123_'
+        }
+
+        serializer = JobSeekerRegisterSerializer(instance=jobseeker, data=update_payload)
+        self.assertTrue(serializer.is_valid())
+        if serializer.is_valid():
+            updated_jobseeker = serializer.save()
+            self.assertTrue(updated_jobseeker.check_password(update_payload['password']))
+
+
+    def test_update_jobseeker_password_not_match(self):
+        jobseeker = JobSeeker.objects.create(
+            email='test@test.com',
+            first_name='John',
+            last_name='Doe',
+            other_names='random',
+            dob='1990-01-01',
+            phone_number='1234567890',
+            nationality='Country',
+            sex='M'
+        )
+        # Create a payload for update with mismatching passwords
+        update_payload = {
+            'email': jobseeker.email,
+            'first_name': jobseeker.first_name,
+            'last_name': jobseeker.last_name,
+            'dob': jobseeker.dob,
+            'phone_number': jobseeker.phone_number,
+            'nationality': jobseeker.nationality,
+            'sex': jobseeker.sex,
+            'password': 'NewTestpassword123_',
+            'password2': 'MismatchedPassword123_'
+    }
+        serializer = JobSeekerRegisterSerializer(instance=jobseeker, data=update_payload)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(serializer.errors['password'][0], 'Password fields do not match')
 
 class ChangePasswordSerializerTestCase(TestCase):
+
+    def setUp(self):
+        self.test_user = User.objects.create_user(email='johndoe5@example.com', password='Password123_')
+        request_factory = APIRequestFactory()
+        self.request = request_factory.get('employer/change-password/')
+        self.request.user = self.test_user
+
     def test_valid_data(self):
         data = {
-            'old_password': 'oldPassword123!',
+            'old_password': 'Password123_',
             'new_password': 'NewPassword123!',
             'confirm_password': 'NewPassword123!'
         }
-        serializer = ChangePasswordSerializer(data=data)
+        serializer = ChangePasswordSerializer(data=data, context={'request' : self.request})
         self.assertTrue(serializer.is_valid())
         self.assertEqual(serializer.validated_data, data)
 
     def test_password_mismatch(self):
         data = {
-            'old_password': 'oldPassword123!',
+            'old_password': 'Password123_',
             'new_password': 'NewPassword123!',
             'confirm_password': 'DifferentPassword123!'
         }
-        serializer = ChangePasswordSerializer(data=data)
+        serializer = ChangePasswordSerializer(data=data, context={'request' : self.request})
         self.assertFalse(serializer.is_valid())
         self.assertIn('non_field_errors', serializer.errors)
         self.assertEqual(serializer.errors['non_field_errors'][0], "New password and confirm password must match")
+    
+    def test_old_password_is_incorrect(self):
+        data = {
+            'old_password': 'WrongOldPassword123_',
+            'new_password': 'NewPassword123!',
+            'confirm_password': 'NewPassword123!'
+        }
+        serializer = ChangePasswordSerializer(data=data, context={'request' : self.request})
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('old_password', serializer.errors)
+        self.assertEqual(serializer.errors['old_password'][0], "Incorrect old password")
     
     def test_weak_password_validation(self):
         data = {
@@ -114,7 +215,7 @@ class ChangePasswordSerializerTestCase(TestCase):
             'new_password': 'password123',
             'confirm_password': 'password123'
         }
-        serializer = ChangePasswordSerializer(data=data)
+        serializer = ChangePasswordSerializer(data=data, context={'request':self.request})
         with self.assertRaises(ValidationError) as context:
             serializer.is_valid(raise_exception=True)
 
