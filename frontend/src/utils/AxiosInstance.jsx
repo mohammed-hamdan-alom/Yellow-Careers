6b1dc7c36fe46592b28b169ec416f479ecbf7ba2
mohammed-hamdan-alom <mohammed.hamdan.alom@gmail.com>
2024-03-27 11:59:13 +0000

    Add environment variables

--- frontend/src/utils/AxiosInstance.jsx
+++ frontend/src/utils/AxiosInstance.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const authTokens = JSON.parse(localStorage.getItem("authTokens"));
-const PROD_BASE_URL = "https://mhalom.pythonanywhere.com/";
-const DEV_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = process.env.API_BASE_URL;
 
 const headers = {
   "Content-Type": "application/json",
@@ -14,7 +13,7 @@ if (authTokens && authTokens.access) {
 }
 
 const AxiosInstance = axios.create({
-  baseURL: DEV_BASE_URL,
+  baseURL: API_BASE_URL,
   timeout: 10000,
   headers: headers,
 });
