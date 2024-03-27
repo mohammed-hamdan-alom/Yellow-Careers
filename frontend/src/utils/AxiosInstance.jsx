31e9ee8b7f4ce2ad095a9cd9f70250d197759be9
Ahmad Ramadan <75549950+ahmadramadan0@users.noreply.github.com>
2024-03-27 15:15:25 +0200

    Fix .env issues

--- frontend/src/utils/AxiosInstance.jsx
+++ frontend/src/utils/AxiosInstance.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const authTokens = JSON.parse(localStorage.getItem("authTokens"));
-const API_BASE_URL = process.env.API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const headers = {
   "Content-Type": "application/json",
-  Accept: "application/json",
+  "Accept": "application/json",
 };
 
 if (authTokens && authTokens.access) {
