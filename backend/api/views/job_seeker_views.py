2b4acd1c64df16df3a7ae433e4af225c0a41fa3d
Muhammad0527 <102260144+Muhammad0527@users.noreply.github.com>
2024-03-28 11:10:03 +0000

    Comment more vires

--- backend/api/views/job_seeker_views.py
+++ backend/api/views/job_seeker_views.py
@@ -9,22 +9,28 @@ from rest_framework import generics
 
 
 class BaseJobSeekerView:
+    '''Base view for the JobSeeker model.'''
     queryset = JobSeeker.objects.all()
     serializer_class = JobSeekerSerializer
 
 class JobSeekerListView(BaseJobSeekerView, generics.ListAPIView):
+    '''List all job seekers.'''
     pass
 
 class JobSeekerRetrieveView(BaseJobSeekerView, generics.RetrieveAPIView):
+    '''Retrieve a job seeker. The job seeker id is passed as a parameter in the url.'''
     pass
 
 class JobSeekerCreateView(BaseJobSeekerView, generics.CreateAPIView):
+    '''Create a job seeker.'''
     pass
 
 class JobSeekerUpdateView(BaseJobSeekerView, generics.RetrieveUpdateDestroyAPIView):
+    '''Update a job seeker. The job seeker id is passed as a parameter in the url.'''
     pass
 
 class JobSeekerFromApplicationRetrieveView(BaseJobSeekerView, generics.RetrieveAPIView):
+    '''Retrieve the job seeker of an application. The application id is passed as a parameter in the url.'''
     def get_object(self):
         application_id = self.kwargs["application_id"]
         application = get_object_or_404(Application, id=application_id)
