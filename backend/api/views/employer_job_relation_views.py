4a1ea59b20cbcf4a6f4fcf5d7c97d75fafd15c19
Ahmad Ramadan <75549950+ahmadramadan0@users.noreply.github.com>
2024-03-28 18:26:25 +0200

    Uncomment send_email

--- backend/api/views/employer_job_relation_views.py
+++ backend/api/views/employer_job_relation_views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from api.models import EmployerJobRelation
 from api.serializers.employer_job_relation_serializer import EmployerJobRelationSerializer
 from django.shortcuts import get_object_or_404
+from api.utils.send_email import send_job_access_notification
 
 class BaseEmployerJobRelationView:
     queryset = EmployerJobRelation.objects.all()
@@ -18,7 +19,7 @@ class EmployerJobRelationCreateView(BaseEmployerJobRelationView, generics.Create
         instance = serializer.save()
         
         is_created_by_employer = EmployerJobRelation.objects.filter(job=instance.job).count() == 1
-        # send_job_access_notification(instance.employer.email, instance.job.title, instance.employer.company.company_name, is_created_by_employer)
+        send_job_access_notification(instance.employer.email, instance.job.title, instance.employer.company.company_name, is_created_by_employer)
 
 class EmployerJobRelationUpdateView(BaseEmployerJobRelationView, generics.RetrieveUpdateDestroyAPIView):
     pass
